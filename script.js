c3a28ad0b9d2a033b8f3a7cb3f2bbe6cb1963e5a
xuanhuiyu <150649631+xuanhuiyu@users.noreply.github.com>
2023-11-30 12:16:39 +0800

    v1

--- script.js
+++ script.js
@@ -1,22 +1,64 @@
 window.onload = function(){
 
     // input step
-    const quantityInput = document.querySelector("#quantity");
-    const decreaseButton = document.querySelector("#decrease");
-    const increaseButton = document.querySelector("#increase");
+    const quantityInput1 = document.querySelector("#quantity1");
+    const decreaseButton1 = document.querySelector("#decrease1");
+    const increaseButton1 = document.querySelector("#increase1");
 
-    decreaseButton.addEventListener("click", () => {
-    let currentQuantity = parseInt(quantityInput.value);
+    const quantityInput2 = document.querySelector("#quantity2");
+    const decreaseButton2 = document.querySelector("#decrease2");
+    const increaseButton2 = document.querySelector("#increase2");
+
+    const quantityInput3 = document.querySelector("#quantity3");
+    const decreaseButton3 = document.querySelector("#decrease3");
+    const increaseButton3 = document.querySelector("#increase3");
+
+    decreaseButton1.addEventListener("click", () => {
+    let currentQuantity = parseInt(quantityInput1.value);
     if (currentQuantity > 1) {
-        quantityInput.value = currentQuantity - 1;
+        quantityInput1.value = currentQuantity - 1;
         // aa = currentQuantity;
     }
     });
     
-    increaseButton.addEventListener("click", () => {
-        let currentQuantity = parseInt(quantityInput.value);
-        quantityInput.value = currentQuantity + 1;
+    increaseButton1.addEventListener("click", () => {
+        let currentQuantity = parseInt(quantityInput1.value);
+        quantityInput1.value = currentQuantity + 1;
         // aa  = currentQuantity;
       });
+
+    decreaseButton2.addEventListener("click", () => {
+        let currentQuantity = parseInt(quantityInput2.value);
+        if (currentQuantity > 1) {
+            quantityInput2.value = currentQuantity - 1;
+            // aa = currentQuantity;
+        }
+    });
+
+    increaseButton2.addEventListener("click", () => {
+        let currentQuantity = parseInt(quantityInput2.value);
+        if  (currentQuantity > 1) {
+            quantityInput2.value = currentQuantity + 1;
+            // aa  = currentQuantity;
+        }
+    });
+
+    decreaseButton3.addEventListener("click", () => {
+        let currentQuantity = parseInt(quantityInput3.value);
+        if (currentQuantity > 1) {
+            quantityInput3.value = currentQuantity - 1;
+            // aa = currentQuantity;
+        }
+    });
+
+    increaseButton3.addEventListener("click", () => {
+        let currentQuantity = parseInt(quantityInput3.value);
+        if  (currentQuantity > 1) {
+            quantityInput3.value = currentQuantity + 1;
+            // aa  = currentQuantity;
+        }
+    });
+      
+
    
 }
